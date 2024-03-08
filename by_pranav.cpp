1f2e74e07b374f164bb8973ff022d0101e9be438
Pranav1433 <161376369+Pranav1433@users.noreply.github.com>
2024-03-08 12:01:09 +0530

    Create by_pranav.cpp

---
+++ by_pranav.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person{
+
+public:
+
+    string club_name;
+    long long id;
+    string name;
+    long long contact;
+    string pos;
+
+    Person(){
+        club_name="empty";
+        id=0;
+        name="empty";
+        contact=0;
+        pos="empty";
+    }    
+
+    ~Person(){
+
+    }
+};
+
+class Chess{
+
+private:
+
+    Person  member;
+
+public:
+
+};
+
+int main(){
+    cout<<"10";
+}
