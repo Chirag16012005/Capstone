3e0ef331eb4791ae616cbca0ce392f0977e2f6fb
Pranav1433 <161376369+Pranav1433@users.noreply.github.com>
2024-03-08 11:54:28 +0530

    Create Person-Object.cpp

---
+++ Person-Object.cpp
@@ -0,0 +1,22 @@
+class Person{
+
+public:
+
+    string club_name;
+    long long id;
+    string name;
+    long long contact;
+    string pos;
+
+    Person(){
+        club_name="empty";
+        id=0;
+        name="empty";
+        contact=0;
+        pos="empty";
+    }    
+
+    ~Person(){
+
+    }
+};
