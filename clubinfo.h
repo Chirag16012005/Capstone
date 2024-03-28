f12f0d9bce246614e2a28873d4e094bac6e0f5ce
Chirag16012005 <202301259@daiict.ac.in>
2024-03-28 17:03:02 +0530

    +File with information of clubs

---
+++ clubinfo.h
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+void radioclub_info()
+{
+    cout<<"Radio Club is a hobby club at DAIICT, Gujarat, India. Radio Club makes podcasts, shows" 
+    <<"interviews and other radio sessions brought to you by the enthusiastic students of the institute"
+    <<"It is a small group of people, who collectively try to emulate the real life radio shows, allowing people to speak their minds and have fun meanwhile."
+     <<"We have a few RJs, who talk about different topics and current events going on in the college.";
+}
+void sambhav_info()
+{
+cout<<"Sambhav means possible. Sambhav, at DA-IICT is a group of students with interest or urge to"
+ <<"bring some positive change in the society. Sambhav organises various activities throughout a year."   
+ <<"Book Sale is one of our main activities where we sell old unused books at heavily discounted prices to students who wish" 
+ <<"to purchase them. We organise visits to nearby Orphanage and Deaf and Dumb School. These visits make students aware of the"
+ <<"society they live in, making them more attached to various sections of the society as well as generate a sense of responsibility"
+ <<".Another major event which we organise is the Blood Donation Camp where we organise a camp in the college" 
+<< "itself, giving an opportunity the youth of today a chance to serve the society. Sambhav strives to develop a positive" 
+ <<"and empathetic attitude in students towards the society which they are a part of. Sambhav grows, adopts and adapts"
+  <<"thus attaining the sense of We Make It Sambhav";
+}
+void businessclub_info()
+{
+    cout<<"The Business Club consist of individuals who possess a genuine enthusiasm for the dynamics of wealth" 
+    <<"creation and a thirst for knowledge, regardless of their current level of expertise."
+    <<"In addition to financial acumen, we have proactive members who can infuse fresh, creative ideas into our community."
+    <<"Our mission is to reshape the business landscape at our university.You can also join the Business Club and Cultivate a Passion"
+     <<"for Finance, Innovation, and Entrepreneurship!";
+}
+void DCEI_info()
+{
+cout<<" Hello all, we are DA-IICT Centre for Entrepreneurship and Incubation. We're here to help you turn your ideas"
+ <<"into reality. As representatives of DCEI, we're passionate about promoting innovation, entrepreneurship, and problem-solving."
+  <<"Join us to ignite your entrepreneurial spirit!Many of you must have seen shows like Shark Tank and hoped to become part"
+  <<"of something like that.Well, no worries, as we have events like At DA Tank where you'll have the chance to pitch your own"
+  <<"entrepreneurial ideas to seasoned industry pros.";
+}
+void CINS_info()
+{
+    cout<<"We are the Cyber Security Club. We are driven by the amazing passion of students of Cyber Information and Network Security"
+    <<"Club. We also organise different cyber security events that promises to test your skills, challenge your "
+    <<"knowledge, and provide an opportunity to learn and connect with fellow enthusiasts. We will take cyber security into storm.";
+}
+void DSClub_info()
+{
+    cout<<"We are a chapter of Google Developer Groups, which has numerous chapters all over the globe. DA-IICT is"
+     <<"one of the very few colleges in India which has its own chapter. We are a group of technology enthusiasts who aim to"
+      <<"inculcate a healthy developing culture in our college community, by acquainting people with the most essential and also"
+      <<"the latest Tools and Technologies.Our prime focus revolves around technologies developed by Google, we do"
+    <<"explore into other open source technologies such as Git. We at GDG, welcome people who have passion for" 
+    <<"technology and want to develop something tangible.";
+}
+void Webkitclub_info()
+{
+    cout<<"The WebKit club is a student community of Web development enthusiasts, interested both in creating and maintaining codes"
+    <<"as well as building and expanding a stronger community of like minded students. As the name suggests ,we are about all things web."
+    <<"The club motivates budding Web developers by organizing technical sessions and camps to introduce and help them learn new"
+    <<"Web Technologies like HTML5, CSS3, JavaScript, ReactJS, VueJS, Firebase and many more. Our premier event, The WebDev Camp"
+    <<"is organized every semester for students interested in learning Web Development by helping them build a production ready" 
+    <<"application from scratch.";
+   
+}
+void filmclub_info()
+{
+    cout<<"By far the most celebrated club, the Film Club occupies a special place in every student heart. The club was started"
+    <<"by a few movie enthusiasts with the aim of providing regular entertainment to DA-IICTians in the form of interesting as"
+    <<"well as enlightening cinema. The club screens a movie the night before every regular holiday (Saturdays and Sundays) and" 
+    <<"Screens Matches(Cricket, Football,etc) at the Open Air Theatre (OAT). The Film Club has a wide range of movies 250 English"
+     <<"170 Hindi and 25 Other, and is also continually being increased regularly. Bears ample testimony to the fact that the club" 
+     <<"can boast not only of quantity, but also of quality. Keeping all interests in mind, the club has something for each student"
+      <<"of DAIICT. From Shutter Island to Guide, from 3 Idiots to Devdas, the movies cater to multiple and varied backgrounds.";
+}
+void Press_info()
+{
+    cout<<"As the journalism cell of DAIICT, The Press Club strives to give voice to the student community and" 
+    <<"aspires to be a podium to lodge campus musings in all its moods. To achieve its maxim, The Press Club engages in a" 
+    <<"number of journalistic undertakings in the campus. Its principal product is Entelechy, DAIICT’s only campus magazine." 
+    <<"Entelechy, the student-run magazine of DA, has over the years and over its editions, morphed into a voice, a power, a passion" 
+    <<"and an endeavor in the verve of those it envelops, and that is what makes Entelechy more than a magazine. Entelechy is, to us,"
+    <<"a concept of volition; it is the autonomy of thought and an enticement to ingenuity. We, at the Press Club, aspire to make a"
+    <<"difference in the littlest way we can";
+}
