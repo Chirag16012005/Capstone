e942aa64ed2be51191ea36fb4a4b7ff272309838
ParthKarena1636 <161446939+ParthKarena1636@users.noreply.github.com>
2024-04-03 00:50:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 #include <string>
-#include "by_pranav.h"
 #include "by_pranav.cpp"
 #include <fstream>
 #include "clubinfo.h"
-#include "addentry.h"
-
 using namespace std;
 
 int main()
 {
-
-    cout << "Welcome to DA_IICT CLUB MANAGER." << endl;
+    std::cout << endl;
+    std::cout << "======================================= Welcome to DA_IICT CLUB MANAGER =======================================" << endl;
     string file_name;
 
-    cout << "Enter the file name from which data will be loaded into the programme. (Use the extension .txt also)" << endl;
-
 label:
-
+    std::cout << endl;
+    std::cout << "Enter the file name from which data will be loaded into the programme. (Use the extension .txt also): ";
     cin >> file_name;
     ifstream fin;
     fin.open(file_name, ios::in);
 
     if (!fin && !fin.eof())
     {
-        cout << "File does not exist or file is empty" << endl;
-
-        cout << "Press 0 to exit or Press 1 to continue and give file name again" << endl;
+        std::cout << endl;
+        std::cout << "File does not exist or file is empty" << endl;
+        std::cout << "Press 0 to exit or Press 1 to continue and give file name again: ";
         int k;
         cin >> k;
 
@@ -44,19 +40,25 @@ label:
 
     list = load_data(file_name); // All the data from file gets loaded into the unordered map
     int b = 1;
-
+    cout << "File Uploaded successfully..!!" << endl;
     while (b == 1)
     {
-        cout << "Please follow the following menu \n 2. All club members of a particular club. \n 3. To get the details of all members."
-             << "\n 4. To get details of club \n 5. To search any club member. \n 6. To become a member in any club \n 7. To leave any club \n 8. To reload data in any file." << endl;
+        std::cout << endl;
+        std::cout << "Please follow the following menu" << endl;
+        std::cout << "--------------------------------" << endl;
+        std::cout << " 1. All club members of a particular club. \n 2. To get the details of all members."
+                  << "\n 3. To get details of club \n 4. To search any club member. \n 5. To become a member in any club \n 6. To leave any club \n 7. To reload data in any file." << endl;
+        std::cout << " 8. to exit" << endl;
+        std::cout << "Enter Choice: ";
         int menu;
         cin >> menu;
         switch (menu)
         {
 
-        case 2:
+        case 1:
         {
-            cout << "Please enter the club name(You may type the name only and no need for the word CLUB, only for developer student club type DSClub)";
+            std::cout << endl;
+            std::cout << "Please enter the club name(You may type the name only and no need for the word CLUB, only for developer student club type DSClub): ";
             string club;
             cin >> club;
             mem_club(list, club);
@@ -64,66 +66,66 @@ label:
             break;
         }
 
-        case 3:
+        case 2:
         {
-            cout << "All the club member details are as follows:" << endl;
+            std::cout << "All the club member details are as follows: " << endl;
             get_all_mem(list);
-
             break;
         }
 
-        case 4:
+        case 3:
         {
-            cout << "Please follow the following menu to fetch information about any club" << endl
-                 << "1.Radio Club  2.Sambhav  3.Business Club  4.DCEI" << endl
-                 << "5.CINS  6.Webkit Club  7.Muse Club  8.Debate Club  9.Music Club" << endl
-                 << "10.Developer Student Club  11.Chess Club  12.Press Club  13.IEEE" << endl
-                 << "14.Film Club  15.Research Club  16.Programming Club  17.PMMC" << endl
-                 << "18.Khelaiya Club  19.Cubing Club  20.Excursion Club  21.BIS club" << endl
-                 << "22.Heritage Club  23.Electronic Hobby Centre  24.Headrush-quizzing" << endl
-                 << "25.DAIICT Theatre Group 26.AI Club" << endl;
+            std::cout << endl;
+            std::cout << "Please follow the following menu to fetch information about any club: " << endl;
+            std::cout << "---------------------------------------------------------------------" << endl;
+            std::cout << left << setw(30) << "1.Radio Club" << setw(30) << "2.Sambhav" << setw(30) << "3.Business Club" << setw(30) << "4.DCEI" << endl
+                      << left << setw(30) << "5.CINS" << setw(30) << "6.Webkit Club" << setw(30) << "7.Muse Club" << setw(30) << "8.Debate Club" << endl
+                      << left << setw(30) << "9.Music Club" << setw(30) << "10.Developer Student Club" << setw(30) << "11.Chess Club" << setw(30) << "12.Press Club" << endl
+                      << left << setw(30) << "13.IEEE" << setw(30) << "14.Film Club" << setw(30) << "15.Research Club" << setw(30) << "16.Programming Club" << endl
+                      << left << setw(30) << "18.Khelaiya Club" << setw(30) << "19.Cubing Club" << setw(30) << "20.Excursion Club" << setw(30) << "21.BIS club" << endl
+                      << left << setw(30) << "17.PMMC" << setw(30) << "22.Heritage Club" << setw(30) << "23.Electronic Hobby Centre" << setw(30) << "24.Headrush-quizzing" << endl
+                      << left << "25.DAIICT Theatre Group" << endl;
 
             int c1;
             do
             {
                 int r;
+                std::cout << "Enter Your choice: ";
                 cin >> r;
-                info(r);
-                cout << "If you again want information, press 1 or Press 0 if you do not want any info" << endl;
+                // info(r);
+                std::cout << "If you again want information, press 1 or Press 0 if you do not want any info: ";
                 cin >> c1;
             } while (c1 == 1);
-
             break;
         }
 
-        case 5:
+        case 4:
         {
             search(list);
-
+            break;
+        }
+        case 5:
+        {
+            // addentry(list);
             break;
         }
         case 6:
         {
-            addentry(list);
+            del_entry(list);
             break;
         }
         case 7:
         {
-            del_entry(list);
-            break;
-        }
-        case 8:
-        {
-            cout << "Please enter the name of file, in which you want to reload data.(Please use extension .txt)" << endl;
+            std::cout << "Please enter the name of file, in which you want to reload data.(Please use extension .txt)" << endl;
             string file;
             cin >> file;
             reload_data(list, file);
             break;
         }
+        case 8:
+        {
+            return 0;
         }
-
-        cout << "If you want to enter into DAIICT club manager , press 1 or you may press 0 to exit" << endl;
-        cin >> b;
+        }
     }
-    return 0;
 }
