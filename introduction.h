ec2a502b1db5c4020ff6b54bc0e287bbe6117a67
Chirag16012005 <161330130+Chirag16012005@users.noreply.github.com>
2024-04-07 00:38:21 +0530

    introduction added

---
+++ introduction.h
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+void intro()
+{
+    cout << "----------------------------------------------------------------------------------------------------------" << endl;
+    cout<<"DAIICT was created as one of the most advanced institute in 2001 which believes in overall development. Along with academics"
+    <<"the focus is also on co-curricular activities. As a part of this, DA-IICT has also many clubs that allows students to enhance"
+    <<"their skills and hobbies along with academics. The clubs at DAIICT works to provide platform for students to showcase their"
+    <<"talent and skills regarding their hobbies. One of our oldest and most prominent events at DAIICT is Synapse."
+    <<"Synapse is a celebration - a celebration of culture, technology and life. It is about realizing the underlying inherent" 
+    <<"junction of the three in an electrifying atmosphere. Similarly, through exciting workshops, impressive events, and "
+    <<"mesmerizing pro-nights, other clubs and their events are a medium through which the youth can express their views; be it through art," 
+    <<"culture, technological skills, knowledge or entertainment.  It also promotes various cultural activities like music, "
+    <<"drama and dance amongst the student community. Some of our clubs are:"<<endl;
+    cout<<"--AI Club--"<<endl;
+    cout<<"Started in 2020, it aims to educate others about AI. EVENT: i.Prompt, Prompt-A-Thon to create AI framework."<<endl;
+    cout<<"--Press Club--"<<endl;
+    cout<<"Started in 2010, it is the main journalism cell of DAIICT. EVENT: Comic Carnival: A Comic Making Challenge. and Entelechy"
+    "-the student run magazine of DA"<<endl;
+    cout<<"--Debate Club--"<<endl;
+    cout<<"We are a group of people who believe in the power of words to bring change, who promote discussion. EVENT: Parliamentary Debates"
+    "to enhance knowledge and dissent"<<endl;
+    cout<<"--Dance Club--"<<endl;
+    cout<<"Dance Club or DADC as it is known, tries to cater the dancing instincts of students.EVENT: WALTZ to electrify the atmosphere"<<endl;
+    cout<<"--DCEI Club--"<<endl;
+    cout<<"We are DA-IICT Centre for Entrepreneurship and Incubation. EVENT: SellOut to show business skills"<<endl;
+    cout<<"--Music Club--"<<endl;
+    cout<<"We aim to develop an interest in students towards music and create a healthy music atmosphere EVENT: RAAGA AND RHAPSODY"<<endl;
+    cout<<"--DTG Club--"<<endl;
+    cout<<"DAIICT Theatre Group is a club that conducts and administers theatrics events of DAIICT.EVENT: Rangmanch - The Drama Night"<<endl;
+    cout<<"--Research Club--"<<endl;
+    cout<<"We aim to work to foster a community within the college for those interested in research. EVENT: i.Papyrus to develop liking for research"<<endl;
+    cout<<"--Chess Club--"<<endl;
+    cout<<"We focus on enhancing the skills of the students having interest in chess. EVENT: Rapid Chess Contest"<<endl;
+    cout<<"--Sambhav--"<<endl;
+    cout<<"Sambhav means possible. Sambhav is a group of students with a motive to bring about some positive change in the society"
+    "EVENT: Blood Donation "<<endl;
+
+    
+}
