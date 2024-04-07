4e42c3878d56775959c713be1fcdd023dbb987f4
Pranav1433 <161376369+Pranav1433@users.noreply.github.com>
2024-04-07 20:52:17 +0530

    color

--- main.cpp
+++ main.cpp
@@ -7,18 +7,44 @@
 #include "intoduction.h"
 using namespace std;
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define BOLD    "\033[1m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define BLACK       "\033[30m"
+#define BOLD_BLACK  "\033[1m\033[30m"
+#define BOLD_RED    "\033[1m\033[31m"
+#define BOLD_GREEN  "\033[1m\033[32m"
+#define BOLD_YELLOW "\033[1m\033[33m"
+#define BOLD_BLUE   "\033[1m\033[34m"
+#define BOLD_MAGENTA "\033[1m\033[35m"
+#define BOLD_CYAN   "\033[1m\033[36m"
+#define BOLD_WHITE  "\033[1m\033[37m"
+#define BRIGHT_BLACK        "\033[90m"
+#define BRIGHT_RED          "\033[91m"
+#define BRIGHT_GREEN        "\033[92m"
+#define BRIGHT_YELLOW       "\033[93m"
+#define BRIGHT_BLUE         "\033[94m"
+#define BRIGHT_MAGENTA      "\033[95m"
+#define BRIGHT_CYAN         "\033[96m"
+#define BRIGHT_WHITE        "\033[97m"
+
+
+using namespace std;
+
 int main()
 {
     std::cout << endl;
-    std::cout << "======================================= Welcome to DA_IICT CLUB MANAGER =======================================" << endl;
-    intro();
+    std::cout <<BOLD_YELLOW<< "======================================= Welcome to DA_IICT CLUB MANAGER =======================================" <<RESET<< endl;
     string file_name;
 
 item:
-    std::cout << endl << endl;
-    std::cout << "There are some of the guidelines before entering into the DAIICT Club Manager"<<endl
-    <<"1. Please never enter the word 'club' while giving club name as input. Only write club name with starting letter as capital."<<endl;
-    <<"2. While entering the name, please follow the format of 'First_name Last_name'."<<endl<<endl;
+    std::cout << endl;
     std::cout << "Enter the file name from which data will be loaded into the programme. (Use the extension .txt also): ";
     cin >> file_name;
     ifstream fin;
@@ -27,8 +53,8 @@ item:
     if (!fin && !fin.eof())
     {
         std::cout << endl;
-        std::cout << "File does not exist or file is empty" << endl;
-        std::cout << "Press 0 to exit or Press 1 to continue and give file name again: ";
+        std::cout <<RED<< "File does not exist or file is empty" <<RESET<< endl;
+        std::cout <<BRIGHT_RED<< "Press 0 to exit or Press 1 to continue and give file name again: ";
         int k;
         cin >> k;
 
@@ -48,33 +74,37 @@ item:
     auto temp = load_data(file_name); // All the data from file gets loaded into the unordered map
 
     list = temp.first;
+
     na_id = temp.second;
+
     int b = 1;
-    cout << "File Uploaded successfully..!!" << endl;
-    unordered_map<string, unordered_multimap<int, Event>> data;
-    data = ev_load();
-    unordered_map<string, unordered_map<string, Person>> par_detials;
-    string s = "participate.txt";
-    par_detials = par_load(s);
+    cout <<GREEN<< "File Uploaded successfully..!!" <<RESET<< endl;
     while (b == 1)
     {
-        std::cout << endl;
-        std::cout << "Please follow the following menu" << endl;
-        std::cout << "--------------------------------" << endl;
-        std::cout << " 1. All club members of a particular club. \n 2. To get the details of all members."
-                  << "\n 3. To get details of club \n 4. To search any club member. \n 5. To become a member in any club \n 6. To leave any club ." << endl;
-        std::cout << " 7. to exit" << endl;
-        std::cout << "Enter Choice: ";
+         std::cout << endl;
+         std::cout <<BOLD_MAGENTA<< "Please follow the following menu" <<RESET<< endl;
+         std::cout <<BOLD_MAGENTA<< "--------------------------------" <<RESET<< endl;
+         std::cout << BLUE << BOLD << "1. All club members of a particular club." << RESET << endl;
+         std::cout << BLUE << BOLD << "2. To get the details of all members." << RESET << endl;
+         std::cout << BLUE << BOLD << "3. To get details of club." << RESET << endl;
+         std::cout << BLUE << BOLD << "4. To search any club member." << RESET << endl;
+         std::cout << BLUE << BOLD << "5. To become a member in any club." << RESET << endl;
+         std::cout << BLUE << BOLD << "6. To leave any club." << RESET << endl;
+         std::cout << BLUE << BOLD << "7. To get detials for Events" << RESET << endl;
+         std::cout << BLUE << BOLD << "8. to edit event detials" << RESET << endl;
+         std::cout << BLUE << BOLD << "9. to Participate any Event" << RESET << endl;
+         std::cout << BLUE << BOLD << "10.To get ditials of festivals" << RESET << endl;
+         std::cout << BLUE << BOLD << "11.to exit" << RESET << endl;
+         std::cout << "Enter Choice: ";
         int menu;
         cin >> menu;
-        cin.ignore();
         switch (menu)
         {
 
         case 1:
         {
             std::cout << endl;
-            std::cout << "Please enter the club name(You may type the name only and no need for the word CLUB, only for developer student club type DSClub): ";
+            std::cout <<BRIGHT_RED<< "Please enter the club name(You may type the name only and no need for the word CLUB, only for developer student club type DSClub): "<<RESET<<endl;
             string club;
             cin >> club;
             mem_club(list, club);
@@ -84,7 +114,7 @@ item:
 
         case 2:
         {
-            std::cout << "All the club member details are as follows: " << endl;
+            std::cout <<YELLOW<< "All the club member details are as follows: " <<<RESET< endl;
             get_all_mem(list);
             break;
         }
@@ -92,8 +122,8 @@ item:
         case 3:
         {
             std::cout << endl;
-            std::cout << "Please follow the following menu to fetch information about any club: " << endl;
-            std::cout << "---------------------------------------------------------------------" << endl;
+            std::cout <<BOLD_BLUE<< "Please follow the following menu to fetch information about any club: " <<RESET<< endl;
+            std::cout <<BOLD_BLUE<< "---------------------------------------------------------------------" <<RESET<< endl;
             std::cout << left << setw(30) << "1.Radio Club" << setw(30) << "2.Sambhav" << setw(30) << "3.Business Club" << setw(30) << "4.DCEI" << endl
                       << left << setw(30) << "5.CINS" << setw(30) << "6.Webkit Club" << setw(30) << "7.Muse Club" << setw(30) << "8.Debate Club" << endl
                       << left << setw(30) << "9.Music Club" << setw(30) << "10.Developer Student Club" << setw(30) << "11.Chess Club" << setw(30) << "12.Press Club" << endl
@@ -106,10 +136,10 @@ item:
             do
             {
                 int r;
-                std::cout << "Enter Your choice: ";
+                std::cout <<BRIGHT_GREEN<< "Enter Your choice: ";
                 cin >> r;
                 info(r);
-                std::cout << "If you again want information, press 1 or Press 0 if you do not want any info: ";
+                std::cout <<BRIGHT_BLUE<< "If you again want information, press 1 or Press 0 if you do not want any info: ";
                 cin >> c1;
             } while (c1 == 1);
             break;
@@ -123,17 +153,6 @@ item:
         case 5:
         {
             addentry(list);
-            break;
-        }
-        case 6:
-        {
-            del_entry(list);
-            break;
-        }
-        case 7:
-        {
-            cout << endl;
-            event(data);
             break;
         }
         case 8:
@@ -150,7 +169,13 @@ item:
         }
         case 10:
         {
-            reload_data(list,file_name);
+            cout << endl;
+            mj_event();
+            break;
+        }
+        case 11:
+        {
+            reload_data(list, file_name);
             return 0;
         }
         default:
